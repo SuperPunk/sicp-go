230567b9b9ee6672910cf88a97829c7deae1f16b
pakuwa <shylunule@gmail.com>
2020-06-16 00:05:11 +0800

    模拟并发执行的不确定性

--- ch03/concurrency.go
+++ ch03/concurrency.go
@@ -1,14 +1,26 @@
 package ch03
 
+import "sync"
+
+// 运行一百万次，会复现出四种结果：101，121，11，100。110不会出现
 func SimulateConcurrency(N int) map[int]bool {
 	m := make(map[int]bool)
-	x := 100
-	go func(x *int) {
-		*x = *x * *x
-	}(&x)
-	go func(x *int) {
-		*x = *x + 1
-	}(&x)
-	m[x] = true
+	var wg sync.WaitGroup
+	for i := 1; i <= N; i++ {
+		x := 10
+		wg.Add(2)
+		go func(x *int) {
+			*x = *x * *x
+			wg.Done()
+		}(&x)
+
+		go func(x *int) {
+			*x = *x + 1
+			wg.Done()
+		}(&x)
+
+		wg.Wait()
+		m[x] = true
+	}
 	return m
 }
--- ch03/concurrency_test.go
+++ ch03/concurrency_test.go
@@ -1,22 +1,13 @@
 package ch03
 
 import (
-	"fmt"
-	"hash/fnv"
-	"math/rand"
+	"log"
 	"testing"
-	"time"
 )
 
 func TestSimulateConcurrency(t *testing.T) {
-	//ret := SimulateConcurrency(10)
-	//fmt.Print(ret)
-
-	h := fnv.New64a()
-	h.Write([]byte("Ares"))
-	rg := rand.New(rand.NewSource(int64(h.Sum64())))
-	fmt.Println(rg.Int63n(int64(time.Second)))
-	fmt.Println(time.Duration(rg.Int63n(int64(time.Second))))
+	ret := SimulateConcurrency(1000000)
+	log.Println(ret)
 }
 
 func TestSimulateCheckpoint(t *testing.T) {
